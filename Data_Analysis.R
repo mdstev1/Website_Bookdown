84f507c8178369dfb69624f9132c2e8433a0961a
mdstev1 <michaeldstevens1@gmail.com>
2020-12-02 23:33:09 -0700

    Further work on the ELM code Specifically: writing code for the A term and the Y term. This involved cleaning up the code and variables a little bit.

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -1,12 +1,11 @@
 load("master.Rda")
-load("master_wells.csv")
 master_wells <- read.csv(file = 'master_wells.csv')
 test_wells <- read.csv(file = 'test_wells.csv')
 
 test_ts_3mon <- data.frame(c())
 for (well in test_wells$mergeOn){
   ts <- filter(master, mergeOn == well)
-  yr_ts <- ts %>%
+  w3mon_ts <- ts %>%
     mutate(date = lubridate::floor_date(date, "3 months"), mergeOn = mergeOn) %>%
     group_by(date, mergeOn) %>%
     summarize(Mean_depth=-1*mean(depth.to.GW..ft.))
@@ -35,7 +34,7 @@ climate_output_sc <- nc_open("pdsi.mon.mean.selfcalibrated.nc")
 print(climate_output_sc)
 
 lat <- ncvar_get(climate_output_sc,"lat")
-lon
+lon <- ncvar_get(climate_output_sc,"lon")
 time <- ncvar_get(climate_output_sc,"time")
 tail(time)
 tunits <- ncatt_get(climate_output_sc,"time","units")
@@ -48,6 +47,8 @@ lon[ncd_lon]
 ncd_lat <- which(abs(lat-well_lat)==min(abs(lat-well_lat)))
 lat[ncd_lat]
 
+
+# This is just code to demonstrate how to work with the dates in the netCDF
 st_date <- as.Date("1800-01-01")
 date <- as.Date("2012-01-01")
 date_diff <- as.numeric((date-st_date)*24)
@@ -76,17 +77,38 @@ X <- mutate(as.data.frame(X), PDSI = X, date = time, .keep = "none")
 X$date <- as.Date(X$date/24, origin = "1800-01-01")
 
 # Create a ts file of the PDSI data with 3 month averages and a time frame lining up with the well data
-ncd_ts <- X %>%
+ncdf_ts <- c()
+ncdf_ts <- X %>%
   mutate(date = lubridate::floor_date(date, "3 months")) %>%
   group_by(date) %>%
   summarize(PDSI=mean(PDSI)) %>%
-  subset(ncd_ts, date %in% test_ts_3mon$date)
+  subset(date %in% test_ts_3mon$date)
+Y <- subset(w3mon_ts, date %in% ncdf_ts$date)
+X <- subset(X, date %in% Y$date)
 
-a <- (ncd_ts$PDSI %*% W1)
+
+# Dot product
+a <- (ncdf_ts$PDSI %*% W1)
 ab <- a + rep(b, each = nrow(a))
 #theta=np.maximum(a,0,a) # basis function
-
+A <- pmax(ab, 0)
 
 # compute the output weights, W2
 # I=np.identity(N) # generate an identity matrix, NxN
-I = diag(5)
+I = diag(1)
+lamb = 100
+lamb*I
+
+# fit using numpy library, uses a Moore-Penrose pseudoinverse of the matrix
+#W2=np.linalg.lstsq(theta.T.dot(theta) + lamb * I, theta.T.dot(Y)[0] 
+# lamb is the regularization parameter (in our case 100)
+  # Y are the observed well data
+  # this is from Eq 8
+W1 <- limsquare_function(t(A) %*% A + lamb*I, t(A) %*% Y)
+
+# Predict – X is a matrix of Earth observation data, W1, W2, and b are saved from above
+  # a=np.dot(X,W1)+b 
+  # theta=np.maximum(a,0,a) # basis function 
+  # Y_imputed= np.dot(theta,W2)
+  
+# Y_imputed needs to be re-scaled before use
